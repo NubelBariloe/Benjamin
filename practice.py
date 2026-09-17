8d22c4fc8a1729004476447c9869f012d77915ec
NubelBariloe <nubelbariloe0@gmail.com>
2026-09-17 20:07:27 +0100

    Update project

--- practice.py
+++ practice.py
@@ -1,36 +1,50 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase,Mapped, mapped_column, Session
+import random
 
-class Base(DeclarativeBase):
-  pass
-
-db = SQLAlchemy(model_class=Base)
-
-# create the app
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-# initialize the app with the extension
-db.init_app(app)
 
-
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str]
-    name: Mapped[str]
-    phone: Mapped[str]
-    password: Mapped[str]
-    course: Mapped[str]
-    registration: Mapped[str] = mapped_column(unique=True)
+    registration = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
-with app.app_context():
-    db.create_all()
-    db.session.add(User(email="nubelbariloe0@gmail.com", id=1, name="nubelbariloe0",
-                         password="123", course="robotics", registration="nubelbariloe0",
-                        phone="09078243611"))
-    db.session.commit()
+    def to_dict(self):
+        return {
+            "registration": self.registration,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password
+        }
 
-    all_books = db.session.query(User).all()
-    print(all_books)
+@app.route('/')
+def index():
+    cafe = User.query.all()
+    random_user = random.choice(cafe)
+    return jsonify (cafe ={
+        "registration": random_user.registration,
+        "username": random_user.username,
+        "email": random_user.email,
+        "password": random_user.password
+
+    })
+
+@app.route('/all')
+def all():
+    cafe = User.query.all()
+    random_user = random.choice(cafe)
+    return jsonify (random_user.to_dict())
+
+@app.route('/a')
+def a():
+    cafes = User.query.all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
+if __name__ == '__main__':
+    app.run(debug=True)
